e1935866f75213e2b0f04f0c20ed2f16fa8af103
Miceuz <mic@hardcore.lt>
2012-11-25 23:10:31 +0100

    send to uart using FIFO buffer and interrupts, send break and mark after break as per DMX packet structure

--- main.c
+++ main.c
@@ -14,12 +14,49 @@
 
 #include "core/uart/uart.h"
 
-#define MAX_DMX_CHANNELS 256
+#define MAX_DMX_CHANNELS 512
 volatile uint8_t dmxChannels[MAX_DMX_CHANNELS]={0};
+
+volatile uint16_t currSendChannel = 0;
+volatile uint8_t updateChannelsEnable = 0;
+
+void UART_IRQHandler(void)
+{
+  uint8_t IIRValue, LSRValue;
+  uint8_t Dummy = Dummy;
+  
+  IIRValue = UART_U0IIR;
+  IIRValue &= ~(UART_U0IIR_IntStatus_MASK); /* skip pending bit in IIR */
+  IIRValue &= UART_U0IIR_IntId_MASK;        /* check bit 1~3, interrupt identification */
+
+  
+  if (IIRValue == UART_U0IIR_IntId_THRE) {
+    if(currSendChannel < MAX_DMX_CHANNELS) {
+      gpioSetValue(CFG_LED_PORT, CFG_LED_PIN, !gpioGetValue(CFG_LED_PORT, CFG_LED_PIN));
+      uint8_t i;
+      for (i = 0 ; i < 16; i++) {
+        UART_U0THR = dmxChannels[currSendChannel++];
+      }
+    } else {
+      gpioSetValue(CFG_LED_PORT, CFG_LED_PIN, CFG_LED_OFF);
+      UART_U0IER = UART_U0IER_THRE_Interrupt_Disabled;
+      updateChannelsEnable = 1;
+    }
+  }
+}
+
+inline void sendBreak() {
+  UART_U0LCR |= UART_U0LCR_Break_Control_Enabled;
+}
+
+inline void clearBreak() {
+  UART_U0LCR &= ~UART_U0LCR_Break_Control_Enabled;
+}
 
 int main(void)
 {
   systemInit();
+  systickConfig(72);//mikrosekunde
   uartInit(250000);
 
   uint32_t currentSecond, lastSecond;
@@ -32,20 +69,32 @@ int main(void)
     dmxChannels[i] = 0;
   }
   
-  
-  while (1)
-  {
-    currentSecond = systickGetSecondsActive();
-    if (currentSecond != lastSecond)
-    {
-      lastSecond = currentSecond;
-      gpioSetValue(CFG_LED_PORT, CFG_LED_PIN, lastSecond % 2);
+  while (1) {
+//    currentSecond = systickGetSecondsActive();
+//    if (currentSecond != lastSecond) {
+//      lastSecond = currentSecond;
+//      gpioSetValue(CFG_LED_PORT, CFG_LED_PIN, lastSecond % 2);
+//    }
+    
+    for (i = 0 ; i < 16; i++) {
+      UART_U0THR = dmxChannels[currSendChannel++];
     }
-
+    
+    while(!updateChannelsEnable) {
+      //nothing
+    }
+    currSendChannel = 0;
     for(i = 0; i < MAX_DMX_CHANNELS; i++) {
       dmxChannels[i] ++;
     }
-    uartSend (dmxChannels, MAX_DMX_CHANNELS);
+    updateChannelsEnable = 0;
+    sendBreak();
+    systickDelay(100);
+    clearBreak();
+    systickDelay(12);
+    UART_U0IER = UART_U0IER_THRE_Interrupt_Enabled;
+
+    //uartSend (dmxChannels, MAX_DMX_CHANNELS);
     
     #ifdef CFG_INTERFACE 
       cmdPoll(); 
