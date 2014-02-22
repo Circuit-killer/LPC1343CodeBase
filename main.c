c1a2ef33e72c2c7d0e12a1083015392d2700dd97
miceuz <mic@wemakethings.net>
2014-02-22 22:30:06 +0100

    chirp testing

--- main.c
+++ main.c
@@ -53,12 +53,69 @@ extern volatile uint8_t   I2CMasterBuffer[I2C_BUFSIZE];
 extern volatile uint8_t   I2CSlaveBuffer[I2C_BUFSIZE];
 extern volatile uint32_t  I2CReadLength, I2CWriteLength;
 
-/**************************************************************************/
-/*! 
-    Main program entry point.  After reset, normal code execution will
-    begin here.
-*/
-/**************************************************************************/
+uint32_t i2c_read(int32_t address, int32_t reg, int32_t bytesToRead) {
+	uint32_t i;
+	
+	if(bytesToRead  > I2C_BUFSIZE) {
+		printf("Too many bytes to read. Max: %d", I2C_BUFSIZE);	
+		return 0;		
+	}
+	
+	if (i2cInit(I2CMASTER) == false) {
+		printf("i2c init failed.\r\n");    /* Fatal error */
+		return 0;
+	}
+
+	for ( i = 0; i < I2C_BUFSIZE; i++ ) {
+	  I2CMasterBuffer[i] = 0x00;
+	}
+
+	I2CWriteLength = 0;
+	I2CReadLength = bytesToRead;
+	I2CMasterBuffer[0] = address;	       // I2C device address
+	I2CMasterBuffer[1] = reg;            // Address (low byte)
+	I2CMasterBuffer[2] = address | 0x01;
+
+	// Transmit command
+	uint32_t result = i2cEngine();
+
+	//printf("%d\r\n", (I2CSlaveBuffer[0] << 8) + I2CSlaveBuffer[1]);
+	return (I2CSlaveBuffer[0] << 8) + I2CSlaveBuffer[1];
+}
+
+void i2c_write(int32_t address, int32_t reg, int32_t bufferLength, uint32_t *buffer) {
+	//uint32_t buffer[62];
+
+	if(bufferLength + 2 > I2C_BUFSIZE) {
+		printf("Too many bytes to send. Max data length: %d", I2C_BUFSIZE - 2);	
+		return;		
+	} 
+
+	if (i2cInit(I2CMASTER) == false) {
+		printf("i2c init failed.\r\n");    /* Fatal error */
+		return;
+	}
+	uint32_t i;
+		
+	for ( i = 0; i < I2C_BUFSIZE; i++ ) {
+	  I2CMasterBuffer[i] = 0x00;
+	}
+
+	I2CWriteLength = 2 + bufferLength;
+	I2CReadLength = 0;
+	I2CMasterBuffer[0] = address;			                // I2C device address
+	I2CMasterBuffer[1] = (reg);            // Address (low byte)
+	
+	for (i = 0; i < bufferLength; i++) {
+	  I2CMasterBuffer[i+2] = buffer[i];
+	}
+//	printf("transmitting %d to %d \r\n", reg, address);
+	// Transmit command
+	uint32_t result = i2cEngine();
+//	printf("transmission result: %d\r\n", result);
+}
+
+
 int main(void)
 {
   // Configure cpu and mandatory peripherals
@@ -68,8 +125,9 @@ int main(void)
   currentSecond = lastSecond = 0;
 
 	uint32_t address = 0x20 << 1;
-	uint32_t reg = 12;
-  
+	uint32_t reg = 0;
+  printf("%d, ", i2c_read(address, reg, 2));
+  systickDelay(1000);
   while (1)
   {
     // Toggle LED once per second
@@ -78,6 +136,12 @@ int main(void)
     {
       lastSecond = currentSecond;
       gpioSetValue(CFG_LED_PORT, CFG_LED_PIN, lastSecond % 2);
+      printf("%d, ", i2c_read(address, reg, 2));
+      systickDelay(1000);
+      i2c_write(address, 3, 0, 0);
+      systickDelay(8000);
+      printf("%d\r\n", i2c_read(address, 4, 2));
+      systickDelay(1000);
     }
     // Poll for CLI input if CFG_INTERFACE is enabled in projectconfig.h
     #ifdef CFG_INTERFACE 
